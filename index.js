011eb09c517d0fabbd60c66b2575c325aa9323bd
Michael Fournier <michaeljfournier5@gmail.com>
2022-02-24 17:56:44 -0500

    commit tree 38e24eeefc882085d3b385a220c7e7924f593bf0

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+function findMatching(drivers, name){
+    const matchingNames = drivers.filter(function (driver){
+        return driver.toUpperCase() === name.toUpperCase();
+    });
+    return matchingNames;
+}
+
+function fuzzyMatch(drivers, startsWith){
+    const matchingNames = drivers.filter(function (driver){
+        return driver.toUpperCase().substring(0,startsWith.length) === startsWith.toUpperCase();
+    });
+    return matchingNames;
+}
+
+function matchName(drivers, name){
+    const matchingNames = drivers.filter(function (driver){
+        return driver.name.toUpperCase() === name.toUpperCase();
+    });
+    return matchingNames;
+}
